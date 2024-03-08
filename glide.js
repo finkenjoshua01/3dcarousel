201b514120a1c0a51e83ef3188641722f025ff5d
finkenjoshua01 <161892231+finkenjoshua01@users.noreply.github.com>
2024-03-08 23:22:15 +0300

    Update glide.js

--- glide.js
+++ glide.js
@@ -3,3 +3,5 @@ import { warn } from './utils/log'
 import { mount } from './core/index'
 import { mergeOptions } from './utils/object'
 import { toInt, isObject, isArray } from './utils/unit'
+
+import EventsBus from './core/event/events-bus'
